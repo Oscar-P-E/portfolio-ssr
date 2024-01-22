fdeb312b8b86a78b755c97bac149420dbf830385
Oscar El <opelm1@gmail.com>
2024-01-22 17:41:40 +1000

    Uploaded webfont

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,12 @@
+// tailwind.config.js
 import { type Config } from "tailwindcss";
-// import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Concourse T4", "ui-sans-serif", "system-ui", "sans-serif"],
-        heading: ["Advocate C51", "sans-serif"],
-        "sans-extralight": [
-          "Concourse T2",
-          "ui-sans-serif",
-          "system-ui",
-          "sans-serif",
-        ],
-        "heading-wider": ["Advocate C63", "sans-serif"],
-        mono: ["ProggyClean Nerd Font", "ui-monospace", "monospace"],
+        mono: ["ProggyVectorRegular", "ui-monospace", "monospace"],
       },
       colors: {
         "neutral-850": "rgb(31, 31, 31)",
