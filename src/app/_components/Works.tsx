9aaf70edb75898a8eec400458b9312e3db0f3d10
Oscar El <opelm1@gmail.com>
2024-02-20 11:49:05 +1000

    Add nlbc to works, update gn images

--- src/app/_components/Works.tsx
+++ src/app/_components/Works.tsx
@@ -27,7 +27,9 @@ export default function Works() {
 
           <div className="fade-in-from-left h-fit">
             <h2 className="text-4xl hover:opacity-80">
-              <a href="http://optimizeelectrical.com.au">Optimize Electrical</a>
+              <a href="https://optimizeelectrical.com.au">
+                Optimize Electrical
+              </a>
             </h2>
             <p className="py-2 text-2xl">
               Not overly fancy, yet professional and modern. User-friendly
@@ -39,7 +41,7 @@ export default function Works() {
               {"> "}
               <a
                 className="underline decoration-dotted hover:opacity-80"
-                href="http://optimizeelectrical.com.au"
+                href="https://optimizeelectrical.com.au"
               >
                 optimizeelectrical.com.au
               </a>
@@ -51,7 +53,7 @@ export default function Works() {
         <div className="flex flex-col gap-12 pt-52 lg:grid lg:grid-cols-3">
           <div className="fade-in-from-left h-fit">
             <h2 className="text-4xl hover:opacity-80">
-              <a href="http://glassninja.com.au">Glass Ninja</a>
+              <a href="https://glassninja.com.au">Glass Ninja</a>
             </h2>
             <p className="py-2 text-2xl">
               A non-standard design almost entirely in pure black and white.
@@ -65,7 +67,7 @@ export default function Works() {
               {"> "}
               <a
                 className="underline decoration-dotted hover:opacity-80"
-                href="http://glassninja.com.au"
+                href="https://glassninja.com.au"
               >
                 glassninja.com.au
               </a>
@@ -77,7 +79,7 @@ export default function Works() {
               {/* <div className="pl-12 pr-11 pt-12 pb-11 bg-gradient-to-tr opacity-90 from-[rgb(240,243,254)] to-[rgb(210,210,225)] rounded-[2rem]"> */}
               <img
                 className="fade-in-from-left rounded-[2rem]"
-                src="/img/works/gn-browser.jpg"
+                src="/img/works/gn-browser-3.jpg"
                 alt="optimize electrical responsive design multiple devices"
               />
               {/* </div> */}
@@ -85,14 +87,14 @@ export default function Works() {
             <div className="pb-12 md:pb-24">
               <img
                 className="fade-in-from-left rounded-[2rem]"
-                src="/img/works/gn-phone.jpg"
+                src="/img/works/gn-phone-2.jpg"
                 alt="optimize electrical mobile phone"
               />
             </div>
             <div className="pb-12 md:pb-24">
               <img
                 className="fade-in-from-left rounded-[2rem]"
-                src="/img/works/gn-phone-contact.jpg"
+                src="/img/works/gn-phone-contact-2.jpg"
                 alt="optimize electrical mobile phone"
               />
             </div>
@@ -100,32 +102,58 @@ export default function Works() {
         </div>
 
         {/* Project 3 */}
-        {/* <div className="flex flex-col-reverse gap-x-12 pt-52 md:grid md:grid-cols-3">
-          <div className="md:col-span-2">
-            <img src="/img/works/gains.png" alt="" />
+        <div className="flex flex-col-reverse gap-12 pt-52 lg:grid lg:grid-cols-3">
+          <div className="lg:col-span-2">
+            <div className="pb-12 md:pb-24">
+              <img
+                className="fade-in-from-left rounded-[2rem]"
+                src="/img/works/nlbc-welcome.jpg"
+                alt="new life baptist church browser mockup contact page"
+              />
+            </div>
+
+            <div className="pb-12 md:pb-24">
+              <img
+                className="fade-in-from-left rounded-[2rem]"
+                src="/img/works/nlbc-laptop.jpg"
+                alt="new life baptist church laptop mockup"
+              />
+            </div>
+
+            <div className="pb-12 md:pb-24">
+              <img
+                className="fade-in-from-left rounded-[2rem]"
+                src="/img/works/nlbc-phone.jpg"
+                alt="new life baptist church phone mockup responsive design"
+              />
+            </div>
           </div>
 
-          <div>
-            <h2 className="text-4xl ">gAIns - AI Training App</h2>
-            <p className="py-2 text-2xl ">
-              Built out of frustration with existing workout apps. gAIns is all
-              about smart, personalized training. It adapts your workout plan
-              based on your progress and goals. No fluff – just a
-              straightforward, data-driven approach to make your workouts count.
-              It&apos;s like having a personal trainer in your pocket, but{" "}
-              <span className="whitespace-nowrap">powered by AI.</span>
+          <div className="fade-in-from-left h-fit">
+            <h2 className="text-4xl hover:opacity-80">
+              <a href="https://nlbc-website.vercel.app">
+                New Life Baptist Church
+              </a>
+            </h2>
+            <p className="py-2 text-2xl">
+              A modern and clean design with a traditional feel. The two main
+              target audiences are people looking for a new church, and people
+              looking for their latest videos online. Key information is
+              displayed clearly and concisely in the About and Contact sections,
+              and the Sermons page features the latest videos from their YouTube
+              channel (automatically kept up to date).
             </p>
             <p className="text-2xl">
               {"> "}
               <a
-                className="underline decoration-dotted"
-                href="https://ai-training-app.vercel.app/"
+                className="underline decoration-dotted hover:opacity-80"
+                href="https://nlbc-website.vercel.app"
               >
-                ai-training-app.vercel.app
+                nlbc-website.vercel.app
               </a>
             </p>
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
